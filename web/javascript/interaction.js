07b5aaca3b71333fbdb75947d4a3fd52332e3b49
Anthony Ashkinadze <ashkinaa@uci.edu>
2025-04-26 15:51:23 -0700

    footer completed

---
+++ web/javascript/interaction.js
@@ -0,0 +1,21 @@
+const accordBtns = document.querySelectorAll(".icon--accord");
+const accordItems = document.querySelectorAll(".item");
+var selected;
+
+console.log(accordBtns);
+
+accordBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    // Remove 'open' from all items
+    accordItems.forEach((item) => item.classList.remove("open"));
+
+    if (btn != selected) {
+      // Add 'open' only to the clicked item's corresponding item
+      accordItems[index].classList.add("open");
+
+      selected = btn;
+    } else {
+      selected = [];
+    }
+  });
+});
